09c563a2e81f1a19f461179774ec290a016066b8
kenpario <108219125+kenpario@users.noreply.github.com>
2026-05-12 10:13:51 +0300

    Day1 basics of python

---
+++ py-learn/typecasting.py
@@ -0,0 +1,20 @@
+name = "Jin"
+age = 24
+gpa = 3.5
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
+
+gpa_as_int = int(gpa)
+print(gpa_as_int)
+age_as_float = float(age)
+print(age_as_float)
+age_as_string = str(age)
+age_as_string += "1"
+print(age_as_string)
+print(type(age_as_string))
+name_as_bool = bool(name)
+print(name_as_bool)
---
+++ py-learn/variables.py
@@ -0,0 +1,25 @@
+#Strings
+first_name = "Tibi"
+
+print(f"Hello, {first_name}!")
+
+#Integers
+age = 25
+
+print(f"{first_name} is {age} years old.")
+
+#Floats
+price = 15.45
+
+print(f"The price is ${price}.")
+
+#Boolean
+if age >= 50:
+    is_elder = True
+else:
+    is_elder = False
+
+if is_elder:
+    print(f"{first_name} is an elder")
+else:
+    print(f"{first_name} is not an elder")
