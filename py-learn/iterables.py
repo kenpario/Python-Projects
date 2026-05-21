7ba80047de8839c1c9f96ce82c55dcb7043d5768
kenpario <108219125+kenpario@users.noreply.github.com>
2026-05-21 11:35:18 +0300

    Day 7 Iterables

---
+++ py-learn/iterables.py
@@ -0,0 +1,21 @@
+# numbers = [1, 2, 3, 4, 5]
+
+# for number in numbers:
+#     print(number, end=" ")
+
+# for number in reversed(numbers):
+#     print(number, end="/")
+
+# fruits = {"apple", "banana", "coconut"}
+
+# for fruit in fruits:
+#     print(fruit, end=" ")
+
+# name = "John Smith"
+
+# for character in name:
+#     print(character, end=" ")
+
+my_dictionary = {"name": "John", "age": 30, "city": "New York"}
+for key, value in my_dictionary.items():
+    print(f"{key}: {value}")
